e253bf2dc61d6add28f24afd9595c7f95f6703ee
Reko <96994029+DevPoli@users.noreply.github.com>
2022-10-02 03:01:06 +0800

    Update abg.lua

--- abg.lua
+++ abg.lua
@@ -1,1 +1,11 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SneosX Hub | AlternativeBattleground", HidePremium = false, IntroText = "SneosX Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+OrionLib:MakeNotification({
+    Name = "AlternativeBattleground | Info",
+    Content = "Please wait for script to loaded!",
+    Image = "",
+    Time = "5",
+})
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xtrey10x/xtrey10x-hub/main/Alternativescript"))()

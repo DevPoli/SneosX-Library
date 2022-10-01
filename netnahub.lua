14b84c3a99b8f379c3a682d4ab05696edf44b1e8
Reko <96994029+DevPoli@users.noreply.github.com>
2022-10-02 04:13:50 +0800

    Update netnahub.lua

--- netnahub.lua
+++ netnahub.lua
@@ -1,1 +1,10 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SneosX Hub | Blox Fruit", HidePremium = false, IntroText = "SneosX Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+OrionLib:MakeNotification({
+    Name = "Blox Fruit | Info!",
+    Content = "Hi there! Due to how door game work, please to our website and execute that script instend!",
+    Image = "",
+    Time = "3",
+})
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/NinoGod/NetnaYay/8d21ce23346c500c93bb8b4a71f7791e4058a70b/startload.lua'))()

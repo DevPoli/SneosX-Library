b5297feb15f77e15bdb6e48947b9dda398e5ceaa
Reko <96994029+DevPoli@users.noreply.github.com>
2022-10-02 02:10:03 +0800

    Update scp3008.lua

--- scp3008.lua
+++ scp3008.lua
@@ -1,1 +1,11 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SneosX Hub | Work At Pizza Place", HidePremium = false, IntroText = "SneosX Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+OrionLib:MakeNotification({
+    Name = "Pizza | Info",
+    Content = "Please wait for script to loaded!",
+    Image = "",
+    Time = "3",
+})
+wait(3)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/502Development/502Lua/main/games/3008.lua'))()

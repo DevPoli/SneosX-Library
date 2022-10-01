acfaa7f35a0166bbb162ebf31bbed54f5fd54ea2
Reko <96994029+DevPoli@users.noreply.github.com>
2022-10-02 01:29:42 +0800

    Update yba.lua

--- yba.lua
+++ yba.lua
@@ -1,1 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/yba"))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SneosX Hub | YBA", HidePremium = false, IntroText = "SneosX Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+OrionLib:MakeNotification({
+    Name = "YBA | Info",
+    Content = "Please wait for script to loaded!",
+    Image = "",
+    Time = "5",
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevPoli/SneosX-Library/main/yba.lua"))()

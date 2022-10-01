bbacd9c190b17efd58ac87dd52161e1c0d04a62e
Reko <96994029+DevPoli@users.noreply.github.com>
2022-10-02 01:57:45 +0800

    Create pizza-hub.lua

---
+++ pizza-hub.lua
@@ -0,0 +1,11 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SneosX Hub | Work at a pizza place", HidePremium = false, IntroText = "SneosX Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+OrionLib:MakeNotification({
+    Name = "Work At a Pizza Place | Info ",
+    Content = "Please wait for script to loaded!",
+    Image = "",
+    Time = "5",
+})
+
+https://raw.githubusercontent.com/DevPoli/SneosX-Library/main/pizza-base.lua
